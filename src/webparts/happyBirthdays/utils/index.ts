7d9eb11a4a16762e9b571498e7dbc3a4cc75c767
Brayan Forero <Brayan Forero>
2022-06-03 11:26:38 -0400

    add fuction clear local storage and key const

---
+++ src/webparts/happyBirthdays/utils/index.ts
@@ -0,0 +1,9 @@
+export const KEY_LOCAL_CONFIG = 'config'
+export const KEY_LOCAL_BIRTHDAYS = 'birthdays'
+export const KEY_LOCAL_GALLERY = 'gallery'
+
+export const clearLocalData = () => {
+  localStorage.removeItem(KEY_LOCAL_CONFIG)
+  localStorage.removeItem(KEY_LOCAL_BIRTHDAYS)
+  localStorage.removeItem(KEY_LOCAL_GALLERY)
+}
